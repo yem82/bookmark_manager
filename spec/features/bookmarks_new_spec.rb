d82cadeddd7310f91f68e94f67b7b087636bad55
Yemi <yemiadebayo60@gmail.com>
2019-05-24 13:02:13 +0100

    Wrap bookmarks in bookmark objects

--- spec/features/bookmarks_new_spec.rb
+++ spec/features/bookmarks_new_spec.rb
@@ -1,8 +1,10 @@
 feature 'adding a new bookmark' do
   scenario 'add a new bookmark' do
-  visit('/bookmarks/new')
-  fill_in 'url', with: 'http://testbookmark.com'
-  click_button('Add')
-  expect(page).to have_content 'http://testbookmark.com'
+    visit('/bookmarks/new')
+    fill_in 'url', with: 'http://testbookmark.com'
+    fill_in 'title', with: 'Test Bookmark'
+    click_button('Add')
+    expect(page).to have_link('Test Bookmark', href: 'http://testbookmark.com')
   end
 end
+
